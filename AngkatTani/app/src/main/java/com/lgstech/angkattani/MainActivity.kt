f8cd130f990a10ea3da2b710c10f90710a92e42f
PUSPA WAHYUNINGTIAS <18102137@ittelkom-pwt.ac.id>
2021-05-19 11:13:40 +0700

    init

---
+++ AngkatTani/app/src/main/java/com/lgstech/angkattani/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lgstech.angkattani
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
